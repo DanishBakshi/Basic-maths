3ddc52192ff17ceacce470afc1b1a305fedb7a4f
DanishBakshi <bakshidanish88@gmail.com>
2025-07-09 18:39:48 +0530

    Add files via upload

---
+++ divide.py
@@ -0,0 +1,8 @@
+print()
+a= int(input("The first number is: "))
+print()
+b= int(input("The second number is: "))
+c=a/b
+print()
+print("The answer is: ", c)
+print()
---
+++ under root.py
@@ -0,0 +1,8 @@
+print()
+a= int(input("The number is: "))
+print()
+b= int(input("The order of underroot : "))
+c=a**(1/b)
+print()
+print("The answer is: ", c)
+print()
